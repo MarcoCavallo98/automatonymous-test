8052ee7c228efb2c0afec520e614a5e761d6b2ed
Marco <cavallo.marco0@gmail.com>
2022-12-27 11:13:45 +0100

    Created controller

---
+++ AutomatonymousTest/AutomatonymousTest.ServiceA/APIResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AutomatonymousTest.ServiceA
+{
+    public class APIResponse<T>
+    {
+        public int ErrorCode { get; set; }
+        public string Description { get; set; }
+        public T Body { get; set; }
+    }
+}
---
+++ AutomatonymousTest/AutomatonymousTest.ServiceA/Controllers/TestSagaController.cs
@@ -0,0 +1,58 @@
+﻿using AutomatonymousTest.Common.Commands;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Mime;
+
+namespace AutomatonymousTest.ServiceA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestSagaController : ControllerBase
+    {
+        private readonly IBus _bus;
+        private readonly ILogger<TestSagaController> _logger;
+
+        public TestSagaController(IBus bus, ILogger<TestSagaController> logger)
+        {
+            _bus = bus;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(APIResponse<string>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(APIResponse<string?>), StatusCodes.Status500InternalServerError)]
+        public IActionResult StartSaga()
+        {
+            try
+            {
+                var id = NewId.NextGuid();
+                _bus.Send<StartTestSaga>(new
+                {
+                    Id = NewId.NextGuid(),
+                    Name = $"Test {Guid.NewGuid()}"
+                });
+
+                _logger.LogInformation($"Starting saga: {id}");
+
+                return Ok(new APIResponse<string> { ErrorCode = -1, Description = "OK", Body = id.ToString() });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(ex, ex.Message);
+
+                return new ContentResult
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError,
+                    ContentType = MediaTypeNames.Application.Json,
+                    Content = JsonConvert.SerializeObject(new APIResponse<string?>
+                    {
+                        ErrorCode = -1,
+                        Description = "internal_error",
+                        Body = null
+                    })
+                };
+            }
+        }
+    }
+}
