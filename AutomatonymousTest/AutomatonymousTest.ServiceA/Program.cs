d2c0cd4ab9278e603983ed9c41b4c0f661d0d650
Marco <cavallo.marco0@gmail.com>
2022-12-27 10:35:05 +0100

    Add configuration for Saga

--- AutomatonymousTest/AutomatonymousTest.ServiceA/Program.cs
+++ AutomatonymousTest/AutomatonymousTest.ServiceA/Program.cs
@@ -1,3 +1,7 @@
+using AutomatonymousTest.ServiceA.Sagas.StateMachines;
+using AutomatonymousTest.ServiceA.Sagas.States;
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +9,36 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMassTransit(cfg =>
+{
+    cfg.SetKebabCaseEndpointNameFormatter();
+    cfg.UsingRabbitMq((cxt, rabbitCfg) => 
+    {
+        rabbitCfg.Host(builder.Configuration["RabbitMq:Host"], x =>
+        {
+            x.Username(builder.Configuration["RabbitMq:Username"]);
+            x.Password(builder.Configuration["RabbitMq:Password"]);
+        });
+
+        rabbitCfg.UseDelayedRedelivery(r => r.Intervals(TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(3), TimeSpan.FromMinutes(5)));
+        rabbitCfg.UseMessageRetry(r => r.Immediate(5));
+        rabbitCfg.UseInMemoryOutbox();
+
+        rabbitCfg.ConfigureEndpoints(cxt);
+    });
+    cfg.AddSagaStateMachine<TestStateMachine, TestState>()
+        .MongoDbRepository(x =>
+        {
+            x.Connection = builder.Configuration["MongoDbSagaConfig:Connection"];
+            x.DatabaseName = builder.Configuration["MongoDbSagaConfig:DatabaseName"];
+        })
+        .Endpoint(x => 
+        {
+            x.Name = "test-saga";
+            x.ConcurrentMessageLimit = 8;
+        });
+});
 
 var app = builder.Build();
 
--- AutomatonymousTest/AutomatonymousTest.ServiceA/Sagas/States/TestState.cs
+++ AutomatonymousTest/AutomatonymousTest.ServiceA/Sagas/States/TestState.cs
@@ -2,10 +2,11 @@
 
 namespace AutomatonymousTest.ServiceA.Sagas.States
 {
-    public class TestState : SagaStateMachineInstance
+    public class TestState : SagaStateMachineInstance, ISagaVersion
     {
         public Guid CorrelationId { get; set; }
         public string CurrentState { get; set; } // For a real world app, we should use int, not string, in order to take less space
         public DateTime? LastStateUpdate { get; set; }
+        public int Version { get; set; }
     }
 }
