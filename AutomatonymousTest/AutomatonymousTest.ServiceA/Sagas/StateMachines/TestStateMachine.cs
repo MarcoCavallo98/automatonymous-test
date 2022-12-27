eadbeb9606598db05f2d2ef39615add294fb3489
Marco <cavallo.marco0@gmail.com>
2022-12-27 09:27:25 +0100

    Created and started implementation of AutomatonymousTest.ServiceA

---
+++ AutomatonymousTest/AutomatonymousTest.ServiceA/Sagas/StateMachines/TestStateMachine.cs
@@ -0,0 +1,24 @@
+﻿using AutomatonymousTest.ServiceA.Sagas.States;
+using MassTransit;
+
+namespace AutomatonymousTest.ServiceA.Sagas.StateMachines
+{
+    public class TestStateMachine : MassTransitStateMachine<TestState>
+    {
+        #region states
+        public State BDone { get; private set; }
+        public State CDone { get; private set; }
+        #endregion
+
+        #region events
+        public Event DoB { get; private set; }
+        public Event DoC { get; private set; }
+        #endregion
+
+        public TestStateMachine()
+        {
+            InstanceState(x => x.CurrentState);
+        }
+
+    }
+}
---
+++ AutomatonymousTest/AutomatonymousTest.ServiceA/Sagas/States/TestState.cs
@@ -0,0 +1,10 @@
+﻿using MassTransit;
+
+namespace AutomatonymousTest.ServiceA.Sagas.States
+{
+    public class TestState : SagaStateMachineInstance
+    {
+        public Guid CorrelationId { get; set; }
+        public string CurrentState { get; set; } // For a real world app, we should use int, not string, in order to take less space
+    }
+}
