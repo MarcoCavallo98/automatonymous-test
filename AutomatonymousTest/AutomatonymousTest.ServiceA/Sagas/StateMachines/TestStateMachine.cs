880c2c713fb64c1dc85a9d87df3c534a5e6c8d03
Marco <cavallo.marco0@gmail.com>
2022-12-27 10:03:11 +0100

    Create state machine for saga

--- AutomatonymousTest/AutomatonymousTest.ServiceA/Sagas/StateMachines/TestStateMachine.cs
+++ AutomatonymousTest/AutomatonymousTest.ServiceA/Sagas/StateMachines/TestStateMachine.cs
@@ -1,4 +1,6 @@
-﻿using AutomatonymousTest.ServiceA.Sagas.States;
+﻿using AutomatonymousTest.Common.Commands;
+using AutomatonymousTest.Common.Events;
+using AutomatonymousTest.ServiceA.Sagas.States;
 using MassTransit;
 
 namespace AutomatonymousTest.ServiceA.Sagas.StateMachines
@@ -6,18 +8,41 @@ namespace AutomatonymousTest.ServiceA.Sagas.StateMachines
     public class TestStateMachine : MassTransitStateMachine<TestState>
     {
         #region states
-        public State BDone { get; private set; }
-        public State CDone { get; private set; }
+        public State DoingB { get; private set; }
+        public State DoingC { get; private set; }
         #endregion
 
-        #region events
-        public Event DoB { get; private set; }
-        public Event DoC { get; private set; }
+        #region events/commands
+        public Event<StartTestSaga> StartTestSagaCommand { get; private set; }
+        public Event<BDone> BDoneEvent { get; private set; }
+        public Event<CDone> CDoneEvent { get; private set; }
         #endregion
 
         public TestStateMachine()
         {
             InstanceState(x => x.CurrentState);
+
+
+            Event(() => StartTestSagaCommand, x => x.CorrelateById(context => context.Message.Id));
+            Event(() => BDoneEvent, x => x.CorrelateById(context => context.Message.ItemId));
+            Event(() => CDoneEvent, x => x.CorrelateById(context => context.Message.ItemId));
+
+            Initially(
+                When(StartTestSagaCommand)
+                    .Then(x => x.Saga.LastStateUpdate = DateTime.UtcNow)
+                    .TransitionTo(DoingB));
+
+            During(DoingB,
+                When(BDoneEvent)
+                    .Then(x => x.Saga.LastStateUpdate = DateTime.UtcNow)
+                    .TransitionTo(DoingC));
+
+            During(DoingC,
+                When(CDoneEvent)
+                    .Then(x => x.Saga.LastStateUpdate = DateTime.UtcNow)
+                    .Finalize());
+
+            SetCompletedWhenFinalized();
         }
 
     }
